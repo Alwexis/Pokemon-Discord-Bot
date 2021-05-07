e615dd08d9f66f86019cd2408b84344ad71a7c22
Alexis <51482182+Alexisikg@users.noreply.github.com>
2021-05-07 11:31:04 -0400

    Update and rename Pokedex.py to Bot.py
    - File renamed from Pokedex.py to Bot.py
    - Some optimizations in the code.

--- src/Bot.py
+++ src/Bot.py
@@ -3,42 +3,34 @@ import discord
 import requests
 import json
 import random
-from funciones import pokemonStats
-from funciones import pokemonInfo
-from funciones import pokemonDesc
-from funciones import pokemonCriar
+
+from Pokedex import *
 
 # Discord & Intents
 client = discord.Client()
 intents = discord.Intents.default()
 intents.members = True
 intents.messages = True
+intents.voice_states = True
 intents.integrations = True
 
 # Evento on_ready
 @client.event
 async def on_ready():
-    print('Logged in as {0.user}'.format(client))
+    print('Inicié sesión con el nombre {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-
+    # Pokedex Command
     if message.content.startswith(';pokedex'):
         # Pokémon & Pokedex
         if len(message.content.split(" ")) <= 1:
-            await message.reply("""Por favor menciona un Pokémon válido. Puedes utilizar el parámetro `-r` para que este sea uno random.
-__`Parámetros`__:
-    `-r` = Pokémon Random
-    `-i` = Información Extendida del Pokémon
-    `-c` = Información de Crianza.""")
+            await message.reply("Por favor menciona un Pokémon válido.\n__`Parámetros`__:\n    `-i` = Información Extendida del Pokémon\n    `-c` = Información de Crianza.",
+            mention_author=False)
         else:
             pokemon = message.content.lower().replace(";pokedex ", "")
-            if "-r" in pokemon:
-                random_pokemon = True
-            else:
-                random_pokemon = False
             if "-c" in pokemon:
                 pokemon = pokemon.replace(" -c", "")
                 display_crianza = True
@@ -53,14 +45,7 @@ __`Parámetros`__:
                 pokemon = pokemon.replace(" ", "-")
             if "'" in pokemon:
                 pokemon = pokemon.replace("'", "")
-            if random_pokemon == False:
-                r = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}")
-            else:
-                r2 = requests.get(f"https://pokeapi.co/api/v2/pokemon?offset=1&limit=1116")
-                request2_a_json = json.dumps(r2.json())
-                pokemon2_json = json.loads(request2_a_json)
-                pokemon = pokemon2_json["results"][random.randint(0, 1115)]["name"]
-                r = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}")
+            r = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}")
             if r.status_code != 200:
                 await message.reply(f"No existe un pokémon llamado {pokemon}", mention_author=False)
             else:
@@ -69,14 +54,11 @@ __`Parámetros`__:
                 stats = pokemonStats(pokemon)
                 info = pokemonInfo(pokemon)
                 pokedex = pokemon_json["id"]
+                color = color_por_tipo(pokemon_json["types"][0]["type"]["name"])
                 desc = pokemonDesc(pokedex)
                 crianza = pokemonCriar(pokedex)
-                if random_pokemon == False:
-                    titulo = f"Pokémon {pokemon} ({pokedex})"
-                else:
-                    titulo = f"(RANDOM) Pokémon {pokemon} ({pokedex})"
             # Embeds
-                embed = discord.Embed(title=titulo, url=f"https://www.pokemon.com/el/pokedex/{pokemon}", description=desc, color=0xeb4034)
+                embed = discord.Embed(title=f"Pokémon {pokemon} ({pokedex})", url=f"https://www.pokemon.com/el/pokedex/{pokemon}", description=desc, color=color)
                 embed.set_author(name=message.author.display_name, url="", icon_url=message.author.avatar_url)
                 embed.set_thumbnail(url=pokemon_json["sprites"]["other"]["official-artwork"]["front_default"])
                 embed.add_field(name="Estadísticas", value=stats, inline=False)
